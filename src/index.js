8b77ada4dda0890aedd2fa3487addb872ec8c3c4
Daniel Roth <daniel.roth02@sap.com>
2016-11-11 16:19:06 +0100

    adding inner definitions to the parent definitions

--- src/index.js
+++ src/index.js
@@ -37,6 +37,13 @@ exports.convert = function (raml) {
     return result;
   });
 
+  //Fix to add the inner definitions present in the schema into parent definitions
+  _.each(jp.nodes(swagger, '$..*["x-definitions"]'), function (innerSchema) {
+    _.each(innerSchema.value, function (schema, name) {
+      swagger.definitions[name] = schema;
+    });
+  });
+
   if ('mediaType' in raml) {
     swagger.consumes = [raml.mediaType];
     swagger.produces = [raml.mediaType];
